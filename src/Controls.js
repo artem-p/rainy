d1ad50b728aa456dce405e356b72fba98e763d40
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-05 22:39:56 +0300

    play pause button

--- src/Controls.js
+++ src/Controls.js
@@ -1,13 +1,19 @@
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import './Controls.css';
+import React, {useState} from 'react';
+import { IconButton } from '@material-ui/core';
 
-
-import React from 'react'
 
 function Controls() {
+    const [play, handlePlay] = useState(false);
+    const togglePlay = () => handlePlay(!play);
+
     return (
         <div className="controls">
-            <PlayCircleOutlineIcon className="controls__playButton"/>
+            <IconButton onClick={togglePlay}>
+                {play ? <PauseCircleOutlineIcon className="controls__playButton" /> : <PlayCircleOutlineIcon className="controls__playButton"/>}
+            </IconButton>
         </div>
     )
 }
