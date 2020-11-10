f5e5bed6aa5d27c41b9f53402ec6f601a8fd0bc0
Artyom Pugachev <artempugachev1@gmail.com>
2020-11-10 16:29:45 +0300

    play sound

--- src/Controls.js
+++ src/Controls.js
@@ -1,13 +1,20 @@
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
 import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
 import './Controls.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { IconButton } from '@material-ui/core';
 
 
 function Controls() {
+    const [rainSound] = useState(new Audio('sound/rainfall.ogg'));
     const [play, handlePlay] = useState(false);
+    
     const togglePlay = () => handlePlay(!play);
+
+    useEffect(() => {
+        play ? rainSound.play() : rainSound.pause();
+        
+    }, [play, rainSound])
 
     return (
         <div className="controls">
